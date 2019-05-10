4b904f69e894b8106d0388a562c95c1fed977880
Lucas Caetano <luhcas.cae@gmail.com>
2019-05-10 11:34:12 -0300

    Styled

--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,20 @@ function append(parent, el) {
     return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
-fetch(url)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) {
-        let authors = data.results; // Get the results
-        return authors.map(function (author) { // Map through the results and for each run the code below
-            let li = createNode('li'), //  Create the elements we need
-                img = createNode('img'),
-                span = createNode('span');
-            img.src = author.picture.medium;  // Add the source of the image to be the src of the img element
-            span.innerHTML = `${author.name.first} ${author.name.last}`; // Make the HTML of our span to be the first and last name of our author
-            append(li, img); // Append all our elements
-            append(li, span);
-            append(ul, li);
-        })
-    });
+function getAuthorList(){
+    fetch(url)
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function(data) {
+            let authors = data.results; // Get the results
+            return authors.map(function (author) { // Map through the results and for each run the code below
+                let li = createNode('li'), //  Create the elements we need
+                    img = createNode('img'),
+                    span = createNode('span');
+                img.src = author.picture.medium;  // Add the source of the image to be the src of the img element
+                span.innerHTML = `${author.name.first} ${author.name.last}`; // Make the HTML of our span to be the first and last name of our author
+                append(li, img); // Append all our elements
+                append(li, span);
+                append(ul, li);
+            })
+        });
+} getAuthorList();
